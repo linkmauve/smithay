87837cde51d64ec9006f30d2cd5a27e5f49f65f9
Drakulix <Drakulix@users.noreply.github.com>
2022-04-25 10:49:37 +0000

    deploy: a878d021e34b2352f80f13852df9a2b2350c41b7

--- smithay/wayland/output/sidebar-items.js
+++ smithay/wayland/output/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["xdg","XDG Output advertising capabilities"]],"struct":[["Mode","An output mode"],["Output","An abstract output."],["PhysicalProperties","The physical properties of an output"]]});
+initSidebarItems({"enum":[["Scale","Describes the scale advertised to clients."]],"mod":[["xdg","XDG Output advertising capabilities"]],"struct":[["Mode","An output mode"],["Output","An abstract output."],["PhysicalProperties","The physical properties of an output"]]});
